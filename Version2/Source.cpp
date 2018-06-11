b744e743b223e03603ffc83c4b8b18d863a44380
Th3WorldEat3r <hendrik.michels@continental-corporation.com>
2018-06-11 14:17:52 +0200

    First

---
+++ Version2/Source.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <iostream>
+#include <stdio.h>
+#include <glad.h>
+#include <glfw3.h>
+
+#include "Shador.hpp"
+
+
+
+int main() {
+
+	readbmp();
+
+	for (int i = 0; i < infoheader.biHeight*infoheader.biWidth; i++) {
+		printf("%03d ", (int)shading[i]);
+	}
+
+
+
+
+
+
+
+
+	if (error != 0) {
+		errorwrite();
+	}
+	free(shading);
+	char stop;
+	std::cin >> stop;
+}
